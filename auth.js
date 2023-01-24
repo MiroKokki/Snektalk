4cd0f81d8b612dba974e476d28af8c657a3b3655
Some Random Slugcat <73141955+Some-Random-Slgcat@users.noreply.github.com>
2023-01-24 15:09:12 -0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -18,7 +18,7 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: 'chat',
+    signInSuccessUrl: '/chat',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
